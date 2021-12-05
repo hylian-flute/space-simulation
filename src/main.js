68a9f50695823ac06c375dea1798bd4bdf89e1b8
hylian-flute <hylian_flute@yahoo.co.jp>
2021-12-06 01:37:16 +0900

    canvasでの表示

--- src/main.js
+++ src/main.js
@@ -1,7 +1,79 @@
-  import init, { PlanetarySystem } from "./pkg/wasm_app.js";
-  await init();
-  const planetarySystem = PlanetarySystem.new();
-  console.log(planetarySystem);
-  console.log(planetarySystem.tick());
-  console.log(planetarySystem.get_planets_positions());
-  console.log(planetarySystem.get_planets_num());
+// @ts-check
+/** @typedef {{get_position(): Float64Array, get_weight(): number}} Planet */
+/** @typedef {{tick: () => void, get_planets: () => Planet[]}} PlanetarySystemD */
+
+// @ts-ignore
+import init, { PlanetarySystem, get_planetary_system_radius } from "./pkg/wasm_app.js";
+
+/**
+ * @template T
+ * @param {unknown} instance 
+ * @param {{new(): T}} Type 
+ */
+function typeGuard(instance, Type) {
+  if (instance instanceof Type) return instance;
+  throw new TypeError();
+}
+
+const CANVAS_SIZE = 640;
+
+const fpsCounter = document.getElementById("fps-counter");
+const canvases = [
+  document.getElementById("planetary-system-display-x"),
+  document.getElementById("planetary-system-display-y"),
+  document.getElementById("planetary-system-display-z"),
+].map(canvas => typeGuard(canvas, HTMLCanvasElement));
+
+/**
+ * @param {PlanetarySystemD} planetarySystem
+ * @param {number} planetarySystemRadius 
+ */
+function displayPlanetarySystem(planetarySystem, planetarySystemRadius) {
+  const contexts = canvases.map(canvas => canvas.getContext("2d"));
+  const dimension = contexts.length;
+  for (const context of contexts) context.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  const planets = planetarySystem.get_planets();
+  const maxWieght = Math.max(...planets.map(planet => planet.get_weight()));
+  for (const planet of planets) {
+    const color = `rgb(255, ${Math.floor(255 * planet.get_weight() / maxWieght)}, 0)`;
+    const scaledPosition = planet
+      .get_position()
+      .map(axisPosition =>
+        Math.floor(
+          (axisPosition + planetarySystemRadius) * CANVAS_SIZE / (2 * planetarySystemRadius)
+        ));
+    for (const [contextIdx, context] of contexts.entries()) {
+      context.fillStyle = color;
+      context.fillRect(
+        scaledPosition[(contextIdx + 1) % dimension],
+        scaledPosition[(contextIdx + 2) % dimension],
+        2,
+        2
+      );
+    }
+  }
+}
+
+/**
+ * @param {PlanetarySystemD} planetarySystem
+ * @param {number} planetarySystemRadius 
+ */
+function tick(planetarySystem, planetarySystemRadius) {
+  const t = Date.now();
+  planetarySystem.tick();
+  displayPlanetarySystem(planetarySystem, planetarySystemRadius);
+  setTimeout(() => {
+    const time = Date.now() - t;
+    fpsCounter.textContent = Math.floor(1000 / time).toString();
+    tick(planetarySystem, planetarySystemRadius);
+  }, 1000 / 10);
+}
+
+// @ts-ignore
+await init();
+/** @type {PlanetarySystemD} */
+const planetarySystem = PlanetarySystem.new();
+/** @type {Number} */
+const planetarySystemRadius = get_planetary_system_radius();
+
+tick(planetarySystem, planetarySystemRadius);
--- wasm-app/src/lib.rs
+++ wasm-app/src/lib.rs
@@ -1,11 +1,10 @@
 use wasm_bindgen::prelude::*;
 
+const DIMENSION: usize = 3;
 const UNIT_TIME: f64 = 60.0 * 60.0 * 24.0; // 一日(秒)
 const PLANETS_NUM: usize = 128;
 
-const PLANETARY_SYSTEM_RADIUS: f64 = 4495060000.0; // 海王星の公転半径(km)
-const I16_COEFFICIENT: f64 = 32767.0 / PLANETARY_SYSTEM_RADIUS;
-const STAR_RADIUS: f64 = 696000.0; // 太陽の半径(km)
+const PLANETARY_SYSTEM_RADIUS: f64 = 227920000.0; // 火星の公転半径(km)
 const STAR_WEIGHT: f64 = 1.989E+30; // 太陽の質量(kg)
 const NEW_PLANET_LARGEST_RADIUS: f64 = 2439.7; // 水星の半径(km)
 const MIN_PLANET_DENSITY: f64 = 687.0E+9 / 2.0; // 土星の密度の半分(kg/km3)
@@ -13,119 +12,143 @@ const MAX_PLANET_DENSITY: f64 = 2.0 * 5.51E+12; // 地球の密度の倍(kg/km3)
 const MAX_PLANET_AXIS_SPEED: f64 = 64.93 * 16.0; // 水星の公転速度の16倍(km/s)
 const GRAVITY_CONSTANT: f64 = 6.6743015E-20; // 万有引力定数(km3/s2kg)
 
-// const TARGET_PLANETS_WEIGHT: f64 = 2589065785521177500000000000.0 / 256.0; // 太陽系の太陽以外の質量の1/256(kg)
-
-struct Vec3 {
-    x: f64,
-    y: f64,
-    z: f64,
+#[wasm_bindgen]
+pub fn get_planetary_system_radius() -> f64 {
+  PLANETARY_SYSTEM_RADIUS
 }
 
-impl Vec3 {
-    pub fn distance(v1: &Vec3, v2: &Vec3) -> f64 {
-        ((v2.x - v1.x).powi(2) + (v2.y - v1.y).powi(2) + (v2.y - v1.y).powi(2)).powf(0.5)
-    }
+pub fn measure_distance(a: &[f64; DIMENSION], b: &[f64; DIMENSION]) -> f64 {
+  let mut distance = 0f64;
+  for i in 0..DIMENSION {
+    distance += (b[i] - a[i]).powi(2);
+  }
+  distance.powf(0.5)
 }
 
+pub fn create_random_vector(axis_min: f64, axis_max: f64) -> [f64; DIMENSION] {
+  let diff = axis_max - axis_min;
+  (0..DIMENSION)
+    .map(|_| diff * js_sys::Math::random() + axis_min)
+    .collect::<Vec<f64>>()
+    .try_into()
+    .unwrap()
+}
+
+#[wasm_bindgen]
+#[derive (Clone)]
 struct Planet {
-    position: Vec3,
-    speed: Vec3,
-    radius: f64,
-    weight: f64,
+  position: [f64; DIMENSION],
+  speed: [f64; DIMENSION],
+  radius: f64,
+  weight: f64,
 }
 
 impl Planet {
-    pub fn new() -> Planet {
-        let radius = NEW_PLANET_LARGEST_RADIUS * js_sys::Math::random();
-        let density = MIN_PLANET_DENSITY + (MAX_PLANET_DENSITY - MIN_PLANET_DENSITY) * js_sys::Math::random();
-        let weight = 4.0 / 3.0 * std::f64::consts::PI * radius.powi(3) * density;
-        Planet {
-            position: Vec3 {
-                x: PLANETARY_SYSTEM_RADIUS * (2.0 * js_sys::Math::random() - 1.0),
-                y: PLANETARY_SYSTEM_RADIUS * (2.0 * js_sys::Math::random() - 1.0),
-                z: PLANETARY_SYSTEM_RADIUS * (2.0 * js_sys::Math::random() - 1.0),
-            },
-            speed: Vec3 {
-                x: MAX_PLANET_AXIS_SPEED * (2.0 * js_sys::Math::random() - 1.0),
-                y: MAX_PLANET_AXIS_SPEED * (2.0 * js_sys::Math::random() - 1.0),
-                z: MAX_PLANET_AXIS_SPEED * (2.0 * js_sys::Math::random() - 1.0),
-            },
-            radius: radius,
-            weight: weight,
-        }
+  pub fn new() -> Planet {
+    let radius = NEW_PLANET_LARGEST_RADIUS * js_sys::Math::random();
+    let density = MIN_PLANET_DENSITY
+      + (MAX_PLANET_DENSITY - MIN_PLANET_DENSITY)
+      * js_sys::Math::random();
+    let weight = 4.0 / 3.0 * std::f64::consts::PI * radius.powi(3) * density;
+    Planet {
+      position: create_random_vector(-PLANETARY_SYSTEM_RADIUS, PLANETARY_SYSTEM_RADIUS),
+      speed: create_random_vector(-MAX_PLANET_AXIS_SPEED, MAX_PLANET_AXIS_SPEED),
+      // speed: [0f64, 0f64, 0f64],
+      radius: radius,
+      weight: weight,
     }
+  }
+}
+
+#[wasm_bindgen]
+impl Planet {
+  pub fn get_position(&self) -> js_sys::Float64Array {
+    js_sys::Float64Array::from(&self.position[..])
+  }
+
+  pub fn get_weight(&self) -> f64 {
+    self.weight
+  }
 }
 
 #[wasm_bindgen]
 pub struct PlanetarySystem {
-    planets: Vec<Planet>,
+  planets: Vec<Planet>,
 }
 
 #[wasm_bindgen]
 impl PlanetarySystem {
-    pub fn new() -> PlanetarySystem {
-        PlanetarySystem {
-            planets: Vec::new()
-        }
+  pub fn new() -> PlanetarySystem {
+    PlanetarySystem {
+      planets: Vec::new()
     }
-    pub fn tick(&mut self) {
-        while self.planets.len() < PLANETS_NUM {
-            self.planets.push(Planet::new());
-        }
+  }
 
-        for planet in &mut self.planets {
-            planet.position.x += UNIT_TIME * planet.speed.x;
-            planet.position.y += UNIT_TIME * planet.speed.y;
-            planet.position.z += UNIT_TIME * planet.speed.z;
-        }
-
-        let mut planets_accelerations: Vec<Vec3> = Vec::new();
-        for planet in &self.planets {
-            let r = Vec3::distance(&Vec3 {
-                x: 0.0,
-                y: 0.0,
-                z: 0.0,
-            }, &planet.position);
-            let a = GRAVITY_CONSTANT * STAR_WEIGHT / r.powi(2);
-            planets_accelerations.push(Vec3 {
-                x: a * planet.position.x / r,
-                y: a * planet.position.y / r,
-                z: a * planet.position.z / r,
-            });
-        }
-
-        for i in 0..(planets_accelerations.len() - 1) {
-            for j in (i + 1)..planets_accelerations.len() {
-                let r = Vec3::distance(&self.planets[i].position, &self.planets[j].position);
-
-                let a_i = GRAVITY_CONSTANT * self.planets[j].weight / r.powi(2);
-                planets_accelerations[i].x += a_i * (self.planets[j].position.x - self.planets[i].position.x) / r;
-                planets_accelerations[i].y += a_i * (self.planets[j].position.y - self.planets[i].position.y) / r;
-                planets_accelerations[i].z += a_i * (self.planets[j].position.z - self.planets[i].position.z) / r;
-
-                let a_j = GRAVITY_CONSTANT * self.planets[i].weight / r.powi(2);
-                planets_accelerations[i].x += a_i * (self.planets[i].position.x - self.planets[j].position.x) / r;
-                planets_accelerations[i].y += a_i * (self.planets[i].position.y - self.planets[j].position.y) / r;
-                planets_accelerations[i].z += a_i * (self.planets[i].position.z - self.planets[j].position.z) / r;
-            }
-        }
-
-        for i in 0..(planets_accelerations.len()) {
-            self.planets[i].speed.x += UNIT_TIME * planets_accelerations[i].x;
-            self.planets[i].speed.y += UNIT_TIME * planets_accelerations[i].y;
-            self.planets[i].speed.z += UNIT_TIME * planets_accelerations[i].z;
-        }
+  pub fn tick(&mut self) {
+    while self.planets.len() < PLANETS_NUM {
+      self.planets.push(Planet::new());
     }
-    pub fn get_planets_positions(&self) -> js_sys::Int16Array {
-        let mut vec = Vec::new();
-        for planet in &self.planets {
-            vec.push((I16_COEFFICIENT * planet.position.x) as i16);
-            vec.push((I16_COEFFICIENT * planet.position.y) as i16);
-            vec.push((I16_COEFFICIENT * planet.position.z) as i16);
+
+    for planet in &mut self.planets {
+      for i in 0..DIMENSION {
+        planet.position[i] += UNIT_TIME * planet.speed[i];
+      }
+    }
+
+    // 恒星(原点)からの重力加速度を加算する
+    let mut planets_accelerations: Vec<[f64; 3]> = Vec::new();
+    for planet in &self.planets {
+      let distance_from_star = measure_distance(&[0f64; DIMENSION], &planet.position);
+      let gravity_accelaration = GRAVITY_CONSTANT * STAR_WEIGHT / distance_from_star.powi(2);
+      let mut accelaration = [0f64; DIMENSION];
+      for dimension_i in 0..DIMENSION {
+        // 重力加速度を分解するために恒星からの距離に対する各軸の距離を掛ける
+        accelaration[dimension_i] =
+          - gravity_accelaration * planet.position[dimension_i] / distance_from_star;
+      }
+      planets_accelerations.push(accelaration);
+    }
+
+    // 各惑星間の重力加速度を加算する
+    for planet_i in 0..(planets_accelerations.len() - 1) {
+      for planet_j in (planet_i + 1)..planets_accelerations.len() {
+        let distance = measure_distance(
+          &self.planets[planet_i].position,
+          &self.planets[planet_j].position
+        );
+
+        let i_gravity_accelaration =
+          GRAVITY_CONSTANT * self.planets[planet_j].weight / distance.powi(2);
+        for dimension_i in 0..DIMENSION {
+          // 重力加速度を分解するために惑星同士の距離に対する各軸に対する各軸の距離を掛ける
+          let axis_distance = self.planets[planet_j].position[dimension_i]
+            - self.planets[planet_i].position[dimension_i];
+          planets_accelerations[planet_i][dimension_i] += i_gravity_accelaration
+            * axis_distance
+            / distance;
         }
-        js_sys::Int16Array::from(&vec[..])
+
+        let j_gravity_accelaration =
+          GRAVITY_CONSTANT * self.planets[planet_i].weight / distance.powi(2);
+        for dimension_i in 0..DIMENSION {
+          let axis_distance = self.planets[planet_i].position[dimension_i]
+            - self.planets[planet_j].position[dimension_i];
+          planets_accelerations[planet_i][dimension_i] +=
+            j_gravity_accelaration * (axis_distance) / distance;
+        }
+      }
     }
-    pub fn get_planets_num(&self) -> usize {
-        self.planets.len()
+
+    for planet_i in 0..(planets_accelerations.len()) {
+      for dimension_i in 0..DIMENSION {
+        self.planets[planet_i].speed[dimension_i] +=
+          UNIT_TIME * planets_accelerations[planet_i][dimension_i];
+      }
     }
+  }
+
+  pub fn get_planets(&self) -> js_sys::Array {
+    let planets = self.planets.clone();
+    planets.into_iter().map(JsValue::from).collect()
+  }
 }
